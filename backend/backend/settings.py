f1a85a7035f1b49be43b5ae595e2401effacb55f
Srinivasan M <msrinivasan401@gmail.com>
2026-04-06 16:59:43 +0530

    Fixed production CORS and CSRF

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -38,11 +38,23 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
-
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "https://kovaidigikite.com",
     "https://www.kovaidigikite.com",
     "https://kovaidigikite-lp8v.vercel.app"
+]
+CORS_ALLOW_HEADERS = [
+    "content-type",
+    "authorization",
+    "x-requested-with",
+    "accept",
+    "origin",
+]
+CORS_ALLOW_METHODS = [
+    "GET",
+    "POST",
+    "OPTIONS",
 ]
 
 ROOT_URLCONF = 'backend.urls'
