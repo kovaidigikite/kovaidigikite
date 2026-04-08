95da54229cf16c1bfe991b2000f4a7b612be2eb2
Srinivasan M <msrinivasan401@gmail.com>
2026-04-08 13:02:05 +0530

    Fix middleware order and CORS

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -29,21 +29,20 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
-CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "https://kovaidigikite.com",
     "https://www.kovaidigikite.com",
-    "https://kovaidigikite-lp8v.vercel.app"
+    "https://kovaidigikite-lp8v.vercel.app",
 ]
 CORS_ALLOW_HEADERS = [
     "content-type",
