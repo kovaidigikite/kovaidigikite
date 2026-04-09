3338c13f8de1381b2a998e63db94790e515ef092
Srinivasan M <msrinivasan401@gmail.com>
2026-04-09 12:10:41 +0530

    Fixed Django bad request for custom domain API

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -22,8 +22,10 @@ DATABASES = {
 ALLOWED_HOSTS = [
     "kovaidigikite.com",
     "www.kovaidigikite.com",
+    "api.kovaidigikite.com",
     "kovaidigikite.onrender.com",
-    "kovaidigikite-lp8v.vercel.app"
+    "localhost",
+    "127.0.0.1",
 ]
 
 INSTALLED_APPS = [
@@ -49,10 +51,10 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-CORS_ALLOWED_ORIGINS = [
+CSRF_TRUSTED_ORIGINS = [
     "https://kovaidigikite.com",
     "https://www.kovaidigikite.com",
-    "https://kovaidigikite-lp8v.vercel.app",
+    "https://api.kovaidigikite.com",
 ]
 CORS_ALLOW_HEADERS = [
     "content-type",
@@ -66,7 +68,9 @@ CORS_ALLOW_METHODS = [
     "POST",
     "OPTIONS",
 ]
-
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+DATA_UPLOAD_MAX_MEMORY_SIZE = 10485760
+FILE_UPLOAD_MAX_MEMORY_SIZE = 10485760
 ROOT_URLCONF = 'backend.urls'
 
 TEMPLATES = [{
